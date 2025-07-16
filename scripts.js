4b55fa01cef2fb85662f007752aceee776cd4e47
amit1262 <amit.sharma1262@gmail.com>
2025-07-16 08:55:49 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab');
+    const contents = document.querySelectorAll('.tab-content');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+    });
+
+    document.querySelector('.tab[onclick*="' + tabId + '"]').classList.add('active');
+    document.getElementById(tabId).classList.add('active');
+}
